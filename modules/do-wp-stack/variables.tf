7585f288d66f03f7d4084b2587f8b6402fb827af
julioesf <julioesf@gmail.com>
2023-07-28 09:40:25 -0300

    Modularization Code

--- modules/do-wp-stack/variables.tf
+++ modules/do-wp-stack/variables.tf
@@ -8,12 +8,17 @@ variable "region" {
 }
 
 variable "wp_vm_count" {
-  type        = number
-  default     = 2
+  type = number
+  default = 2
   description = "Quantity VMs of Wordpress"
 
   validation {
     condition     = var.wp_vm_count > 1
     error_message = "Minimun 2 Machines are needed"
   }
+}
+
+variable "vms_ssh" {
+  type = string
+  description = "SSH Keys to VMs"
 }
